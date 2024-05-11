a6560a441d5dc1ff38625548bd7bddfe13dcae5b
maxmin2127 <mnbernales@gbox.adnu.edu.ph>
2024-05-12 00:50:44 +0800

    Update: Connected the project to PostgreSQL Database

--- babykalmaN1Am/my_app_n1am/my_app_n1am/settings.py
+++ babykalmaN1Am/my_app_n1am/my_app_n1am/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'my_app_n1am.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'test1',
+        'USER': 'member',
+        'PASSWORD': 'member'
     }
 }
 
